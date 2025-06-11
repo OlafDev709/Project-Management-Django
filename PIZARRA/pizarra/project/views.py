78583226a23e00765855481402810255705972cb
oladavis <oladavis425@gmail.com>
2025-06-11 08:22:43 -0700

    Add edit project function

--- PIZARRA/pizarra/project/views.py
+++ PIZARRA/pizarra/project/views.py
@@ -8,9 +8,14 @@ def projects(request):
     projects = Project.objects.filter(created_by=request.user)
     return render(request, 'project/projects.html', {'projects': projects})
 
+@login_required
+def project(request, pk):
+    project = Project.objects.filter(created_by=request.user).get(id=pk)
+    return render(request, 'project/project.html', {'project': project})
+
 
 @login_required
-def add_project(request):
+def add(request):
     if request.method == 'POST':
         name = request.POST.get('name','')
         description = request.POST.get('description','')
@@ -21,5 +26,22 @@ def add_project(request):
         else:
             print('Not valid')
         project.save()
-        return render(request, 'project/add_project.html', {'message': 'Project added successfully'})
+        return render(request, 'project/add_project.html', {'message': 'Please create project.'})
     return render(request, 'project/add.html')
+
+@login_required
+def edit(request, pk):
+    project = Project.objects.filter(created_by = request.user).get(id=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            project.name = name
+            project.description = description
+            project.save()
+
+            return redirect('/projects/')
+        
+    return render(request, 'project/edit.html', {'project': project})
