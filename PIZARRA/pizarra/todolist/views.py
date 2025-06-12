668661feb176e0b50b7cef2fe7640cb57d9c040d
oladavis <oladavis425@gmail.com>
2025-06-11 18:07:30 -0700

    add todolist page

--- PIZARRA/pizarra/todolist/views.py
+++ PIZARRA/pizarra/todolist/views.py
@@ -6,6 +6,13 @@ from django.shortcuts import redirect
 # Create your views here.
 
 @login_required
+def todolist(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=pk)
+
+    return render(request, 'todolist/todolist.html', {'todolist': todolist, 'project': project})
+
+@login_required
 def add(request, project_id):
     project = Project.objects.filter(created_by=request.user).get(pk = project_id)
 
@@ -14,8 +21,25 @@ def add(request, project_id):
         description = request.POST.get('description', '')
 
         if name:
-            todolist = Todolist.objects.create(name=name, description=description, created_by=request.user, project_id=project_id)
+            Todolist.objects.create(name=name, description=description, created_by=request.user, project_id=project_id)
             return redirect(f'/projects/{project_id}/')
         else:
             print('Not valid')
-    return render(request, 'todolist/add.html', {'project': project})
+    return render(request, 'todolist/add.html')
+
+@login_required
+def edit(request, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            project.name = name
+            project.description = description
+            project.save()
+            return redirect(f'/projects/{pk}/')
+        else:
+            print('Not valid')
+    return render(request, 'project/edit.html', {'project': project})
