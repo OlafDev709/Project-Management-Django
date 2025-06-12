c6ab38723f34b0b6656bf5572a61ae36a4b7e2bb
oladavis <oladavis425@gmail.com>
2025-06-11 18:33:19 -0700

    add edit todolist page

--- PIZARRA/pizarra/todolist/views.py
+++ PIZARRA/pizarra/todolist/views.py
@@ -21,25 +21,25 @@ def add(request, project_id):
         description = request.POST.get('description', '')
 
         if name:
-            Todolist.objects.create(name=name, description=description, created_by=request.user, project_id=project_id)
-            return redirect(f'/projects/{project_id}/')
+            todolist = Todolist.objects.create(name=name, description=description, created_by=request.user, project_id=project_id)
+            return redirect(f'/projects/{project_id}/{todolist.id}')
         else:
             print('Not valid')
     return render(request, 'todolist/add.html')
 
 @login_required
-def edit(request, pk):
-    project = Project.objects.filter(created_by=request.user).get(pk=pk)
+def edit(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=pk)
 
     if request.method == 'POST':
         name = request.POST.get('name', '')
         description = request.POST.get('description', '')
 
         if name:
-            project.name = name
-            project.description = description
-            project.save()
-            return redirect(f'/projects/{pk}/')
-        else:
-            print('Not valid')
-    return render(request, 'project/edit.html', {'project': project})
+            todolist.name = name
+            todolist.description = description
+            todolist.save()
+            return redirect(f'/projects/{project_id}/{pk}')
+
+    return render(request, 'todolist/edit.html', {'project': project, 'todolist': todolist})
